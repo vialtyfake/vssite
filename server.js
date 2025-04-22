6839434705e7405621b8264ab38442d62e15d3e0
Nicholas Russ <vialtyreal@gmail.com>
2025-04-22 13:21:57 +0300

    Fix static file server and apply CSS cleanup

--- server.js
+++ server.js
@@ -2,14 +2,31 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimeTypes = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+};
+
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'index.html');
+  // Default to index.html if root is requested
+  let filePath = req.url === '/' ? '/index.html' : req.url;
+  filePath = path.join(__dirname, filePath);
+
+  const ext = path.extname(filePath);
+  const contentType = mimeTypes[ext] || 'application/octet-stream';
+
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      res.writeHead(500);
-      res.end('Server error');
+      res.writeHead(404);
+      res.end('File not found');
     } else {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { 'Content-Type': contentType });
       res.end(content, 'utf-8');
     }
   });
